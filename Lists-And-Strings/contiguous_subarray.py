164b86de323ca53a7f9dbc8c7b8392d3d7a04e9d
Preetamdevji <111443852+Preetamdevji@users.noreply.github.com>
2025-04-15 17:42:22 +0500

    update

--- Lists-And-Strings/contiguous_subarray.py
+++ Lists-And-Strings/contiguous_subarray.py
@@ -1,14 +1,16 @@
 # Task is to find the contiguous subarray (must be one piece, no skipping elements) that has the maximum sum — and return that sum.
 
-numbers = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
 def checkMaximumNumber(numbers):
-    currentNumber = 0
-    maxSum = 0
-    for nums in numbers:
+    currentNumber = numbers[0]
+    maxSum = numbers[0]
+
+    for nums in numbers[1:]:
         currentNumber = max(currentNumber + nums, nums)
-        maxSum = max(maxSum, currentNumber)
-        print(maxSum,currentNumber)
+        maxSum = max(maxSum , currentNumber)
 
+    return maxSum
 
-checkMaximumNumber(numbers)
+numbers = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+results = checkMaximumNumber(numbers)
+print("Maximum number: ", results)
