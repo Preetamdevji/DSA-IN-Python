6c3f80458afed3b2335b0347cae396e54651b385
Preetamdevji <111443852+Preetamdevji@users.noreply.github.com>
2025-05-09 21:07:17 +0500

    complete brute force approach longest-sub-string

--- TwoPointer+SlidingWindow/04_problem.py
+++ TwoPointer+SlidingWindow/04_problem.py
@@ -1,15 +1,55 @@
 # Longest Substring Without Repeating Chars
+# brute force approach
+
+
+# findString = "abcabcbb"
+
+# def longestSubString(findString):
+
+#     try:
+#             longestString = ""
+#             n = len(findString)
+#             for i in range(n):
+#                 for j in range(i + 1, n + 1):
+#                     subStr = findString[i:j]
+#                     uniqueSet = set(subStr)
+#                     if len(subStr) == len(uniqueSet):
+#                         if len(subStr) > len(longestString):
+#                             longestString = subStr
+#                             print("uni",longestString)
+          
+#     except Exception as err:
+#          print(f"An error occurred: {err}")
+
+# longestSubString(findString)
+
+
+
+# brute force approach
+
+
+
 
 findString = "abcabcbb"
 
-def longestSubString(findString):
+def longestSubString():
+    try:
+        right = 0
+        left = 0
+        maxLength = 0
+        result = ""
 
-    n = len(findString)
-    for i in range(n):
-        for j in range(i + 1, n + 1):
-            subStr = findString(n[i:j])
-            print("subStr", subStr)
-            
-        
+        strLenth = len(findString)
+        for i in range(strLenth):
+            window = findString[left:right + 1]
+            right += 1
+            uniqueSet = set(window)
+            if len(window) == len(uniqueSet):
+                if len(window) > maxLength:
+                    maxLength = len(window)
+                    result = window
+        print("res", result)
+    except Exception as err:
+        print(f"Error Occured", {err})
 
-longestSubString(findString)
+longestSubString()
