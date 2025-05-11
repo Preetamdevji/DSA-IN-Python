d21525c004538e9ac9906c7016d29e9ed34a9ce7
Preetamdevji <111443852+Preetamdevji@users.noreply.github.com>
2025-05-11 16:21:38 +0500

    update this with hashing approach

--- Hashing-Dictionary-Set/01_problem.py
+++ Hashing-Dictionary-Set/01_problem.py
@@ -1,21 +1,46 @@
 # Brute Force Approach
 
-nums = [1, 2, 3, 4, 1]
-nums = [1, 2, 3, 4, 5]
+# nums = [1, 2, 3, 4, 1]
+# # nums = [1, 2, 3, 4, 5]
 
 
-def containDuplicate(nums):
+# def containDuplicate(nums):
 
-    try:
+#     try:
     
-        n = len(nums)
-        for i in range(n):
-            for j in range(i + 1 , n):
-                if nums[i] == nums[j]:
-                    return True
-        return False
-    except Exception as err:
-        print(f"Error Occured {err}")    
+#         n = len(nums)
+#         for i in range(n):
+#             for j in range(i + 1 , n):
+#                 if nums[i] == nums[j]:
+#                     return True
+#         return False
+#     except Exception as err:
+#         print(f"Error Occured {err}")    
    
 
-print(containDuplicate(nums))
+# print(containDuplicate(nums))
+
+
+# hashing approach
+# HashSet Method
+# time complexity O(n)
+
+nums = [1, 2, 3, 4, 1]
+
+def checkingDuplicate(nums):
+    try:
+        uniqueElement = set()
+        n = len(nums)
+        print(uniqueElement)
+        for i in range(n):
+            print(f"Element: {nums[i]}, Hash: {hash(nums[i])}")
+            if nums[i] in uniqueElement:
+                return True
+            else:
+                uniqueElement.add(nums[i])
+        return False
+            
+    except Exception as err:
+        print(f"Error Occured {err}")
+
+print(checkingDuplicate(nums))
